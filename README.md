954bd9fe562e7e101c78d4d34a878333b577d1b6
MonstersBY <monsters.alex0@gmail.com>
2024-03-26 11:02:28 +0300

    start: init project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# MaxMalevich
+##### Создать новую ветку
+
+    git checkout -b "New Branch"
+
+##### Взять актуальную версию с main
+
+    git checkout main
+    git fetch
+    git pull
+    git checkout "Your Branch"
+    git pull
+    git rebase origin/main
---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+import 'swiper/css';
+import 'swiper/css/bundle';
+import '../scss/index.scss';
+
+import './pages/home';
+import './utils/swiper';
+
+const btnTop = document.querySelector('.btn_top');
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 100) {
+		btnTop.style.display = 'flex';
+	} else {
+		btnTop.style.display = 'none';
+	}
+});
+
+btnTop.addEventListener('click', () => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth',
+	});
+});
